22f0fb1dea89121d7dd3bb0bcf57234c649d497a
drumilbhati <drumilbhati5@gmail.com>
2026-09-12 16:38:41 +0530

    Remove completed Kafka implementation TODOs

--- src/main/java/com/periscope/kafka/KafkaChangePublisher.java
+++ src/main/java/com/periscope/kafka/KafkaChangePublisher.java
@@ -22,39 +22,24 @@ public class KafkaChangePublisher implements AutoCloseable {
         
         Properties props = new Properties();
         
-        // TODO 1: Set the bootstrap servers from the config
-        // Hint: props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, config.bootstrapServers());
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, config.bootstrapServers());
 
-        // TODO 2: Set strict delivery guarantees (acks=all and enable idempotence)
-        // Hint: props.put(ProducerConfig.ACKS_CONFIG, "all");
-        // Hint: props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
         props.put(ProducerConfig.ACKS_CONFIG, "all");
         props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
 
-        // TODO 3: Configure serializers. Key is String, Value is byte[].
-        // Hint: props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        // Hint: props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
 
-        // TODO 4: Initialize the 'producer' field with the configured properties
-        // Hint: this.producer = new KafkaProducer<>(props);
         this.producer = new KafkaProducer<>(props);
     }
 
     public java.util.concurrent.CompletableFuture<Void> publish(String topic, String key, com.periscope.model.ChangeEvent event) {
-        // TODO 5: Use 'eventSerializer' to convert the event to a byte[]
         byte[] data = eventSerializer.serialize(event);
 
-        // TODO 6: Create a new ProducerRecord<String, byte[]>(topic, key, serializedEventBytes)
         ProducerRecord<String, byte[]> record = new ProducerRecord<>(topic, key, data);
 
-        // TODO 7: Create a new CompletableFuture<Void> called 'future'
         CompletableFuture<Void> future = new CompletableFuture<>();
 
-        // TODO 8: Send the record asynchronously. 
-        // Hint: producer.send(record, (metadata, exception) -> {
         //           if (exception != null) {
         //               future.completeExceptionally(exception);
         //           } else {
@@ -69,18 +54,13 @@ public class KafkaChangePublisher implements AutoCloseable {
             }
         });
 
-        // TODO 9: Return the future
         return future;
     }
 
     @Override
     public void close() {
-        // TODO 10: Flush the producer to ensure any buffered messages are sent to the network.
-        // Hint: producer.flush();
         producer.flush();
 
-        // TODO 11: Close the producer to release resources.
-        // Hint: producer.close();
         producer.close();
     }
 }
--- src/main/java/com/periscope/kafka/TopicRouter.java
+++ src/main/java/com/periscope/kafka/TopicRouter.java
@@ -18,9 +18,7 @@ public class TopicRouter {
 	public record RoutingDecision(String topic, String key) {}
 
 	public RoutingDecision route(ChangeEvent event) {
-		// TODO 1: Construct the topic name.
 		// Format should be: prefix.schemaName.tableName
-		// Hint: String topic = String.format("%s.%s.%s", topicPrefix, event.schemaName(), event.tableName());
 		String topic = String.format(
 			"%s.%s.%s",
 			topicPrefix,
@@ -28,15 +26,12 @@ public class TopicRouter {
 			event.tableName()
 		);
 
-		// TODO 2: Extract the primary key to use as the Kafka partition key.
-		// Hint: Use event.getPrimaryKeyValue(). If it's not null, convert it to a String.
 		// If it is null, just leave the key as null (Kafka will handle it).
 		String key = null;
 		if (event.getPrimaryKeyValue() != null) {
 			key = event.getPrimaryKeyValue().toString();
 		}
 
-		// TODO 3: Return a new RoutingDecision(topic, key)
 		return new RoutingDecision(topic, key);
 	}
 }
